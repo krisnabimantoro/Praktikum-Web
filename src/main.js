f5a8b0ae4e7a711963e086559d54276e729b6ec6
Krisna Bimantoro <krisnabmntr@gmail.com>
2024-12-13 09:34:18 +0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// import { createApp } from 'vue'
+// import './style.css'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+import { createApp } from 'vue';
+//import component App
+import App from './App.vue';
+//import config router
+import router from './router'
+//create App Vue
+const app = createApp(App);
+//gunakan "router" di Vue dengan plugin "use"
+app.use(router);
+app.mount('#app');
